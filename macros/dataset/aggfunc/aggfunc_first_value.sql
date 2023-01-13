08b547d08aafdf36e918f11ddc71a67f68c15d9a
bcodell <bryce.codell@24analytics.co>
2023-01-13 13:20:27 -0500

    handle nulls

--- macros/dataset/aggfunc/aggfunc_first_value.sql
+++ macros/dataset/aggfunc/aggfunc_first_value.sql
@@ -5,13 +5,13 @@
 {%- macro default__aggfunc_first_value(column, activity_name, table_alias, attribute_data_type) -%}
 {%- set activity_col = activity_name~'_activity_at' -%}
 {%- set delimiter = ';.,;' -%}
-cast(split_part(
+cast(nullif(split_part(
             min(
                 cast({{table_alias}}.{{activity_col}} as varchar)
                 || '{{delimiter}}'
-                || cast({{column}} as varchar)
+                || coalesce(cast({{column}} as {{type_string()}}), '')
             ),
             '{{delimiter}}',
             2
-        ) as {{attribute_data_type}})
+        ), '') as {{attribute_data_type}})
 {%- endmacro -%}
