d0c94c3cd9d343b2a14512244e04b2b8577f8404
bcodell <bryce.codell@24analytics.co>
2023-04-19 17:31:04 -0400

    fix typo

--- macros/dataset/relationship/join_custom.sql
+++ macros/dataset/relationship/join_custom.sql
@@ -8,11 +8,11 @@
     before_timestamp=none
 ) %}
 {% if after_timestamp is not none -%}
-{%- set after_timestap_formatted = after_timestamp.format(primary_activity_at=primary_cte~'.'~primary_activity~'_activity_at') -%}
+{%- set after_timestamp_formatted = after_timestamp.format(primary_activity_at=primary_cte~'.'~primary_activity~'_activity_at') -%}
 and {{secondary_alias}}.{{secondary_activity}}_activity_at >= {{after_timestamp_formatted}}
 {% endif %}
 {% if before_timestamp is not none -%}
-{%- set before_timestap_formatted = before_timestamp.format(primary_activity_at=primary_cte~'.'~primary_activity~'_activity_at') -%}
+{%- set before_timestamp_formatted = before_timestamp.format(primary_activity_at=primary_cte~'.'~primary_activity~'_activity_at') -%}
 and {{secondary_alias}}.{{secondary_activity}}_activity_at <= {{before_timestamp_formatted}}
 {% endif %}
 {% endmacro %}
